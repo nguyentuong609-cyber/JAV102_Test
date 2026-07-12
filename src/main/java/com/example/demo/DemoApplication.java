4554f8b08b57e74ab4d674b68bc2f10a2627778b
Tony <nguyentuong609@gmail.com>
2026-07-12 21:12:07 +0700

    Add database schema script

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication app = new SpringApplication(DemoApplication.class);
 		app.setHeadless(false);
 		app.run( args);
